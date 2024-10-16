b8edc75c330c4a367ed3b9768469fdaff5119b6e
CodedCole <106887848+CodedCole@users.noreply.github.com>
2024-10-15 22:18:59 -0500

    WTT-47: Get Retreived Data from Device into a text file

--- KeysightDAQProject/Program.cs
+++ KeysightDAQProject/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Ivi.Visa;
 using Ivi.Visa.Interop;
 using System.Linq;
+using System.Text;
 //using System.Linq.Expressions;
 //using Keysight.Visa;
 
@@ -23,23 +24,42 @@ class Program
             connection.IO = (IMessage)manager.Open(devices[0], AccessMode.NO_LOCK, 0, "");
             connection.IO.Clear();
             connection.WriteString("*IDN?", true);
-            string result = connection.ReadString();
-            Console.WriteLine(result);
-            connection.WriteString("SYSTem:BEEPer:STATe OFF", true);
+            string result1 = connection.ReadString();
+            Console.WriteLine(result1);
+            connection.WriteString("SYSTem:BEEPer:STATe Off", true);
             connection.WriteString("SYSTem:BEEPer:STATe?", true);
             string doesClick = connection.ReadString();
             Console.WriteLine(doesClick);
-            var measureTask = async () =>
+            connection.IO.Clear();
+            connection.WriteString("MEAS:VOLT:DC? 1mV,0.00001,(@301)", true);
+            string result2 = connection.ReadString();
+            Console.WriteLine(result2);
+            connection.IO.Clear();
+            connection.WriteString("CONF:VOLT:DC 1mV,0.00001,(@301)", true);
+            connection.WriteString("FORM:READ:TIME ON", true);
+            connection.WriteString("FORM:READ:TIME:TYPE ABS", true);
+            //string result2 = connection.ReadString();
+            Console.WriteLine("CONF:VOLT:DC");
+            var measureTask = () =>
             {
-                connection.IO.Clear();
-                connection.WriteString("MEAS:VOLT:DC? 1mV,0.00001,(@301)", true);
-                string result2 = connection.ReadString();
-                Console.WriteLine(result2);
-                await Task.Delay(10);
-                return result2;
+                connection.WriteString($"READ?");
+                string result3 = connection.ReadString();
+                Console.WriteLine($"{result3}");
+                //await Task.Delay(10);
+                return result3;
             };
-            for (int i = 0; i < 30; i++)
-                Task.Run(measureTask).Wait();
+
+            List<string> resultList = new List<string>();
+            for (int i = 0; i < 1000; i++)
+            {
+                resultList.Add(measureTask());
+            }
+            FileStream fs = File.Open(Path.Combine(Environment.CurrentDirectory,"test.txt"), FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            foreach (var result in resultList)
+            {
+                fs.Write(Encoding.UTF8.GetBytes(result));
+            }
+            fs.Close();
             //connection.IO.Clear();
             //connection.WriteString("ACQ:VOLT:DC MIN,DEF,DEF,(@301)", true);
             //connection.WriteString("MEAS:VOLT:DC? 1mV,0.00001,(@301)", true);
