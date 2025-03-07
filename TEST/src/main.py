77e57059553e2db9a2453f55fc88f2c753262990
Mekeal <brownmekeal@gmail.com>
2025-03-07 16:23:54 -0600

    Changed directory structure to be cleaner

--- TEST/src/main.py
+++ TEST/src/main.py
@@ -1,72 +1,10 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
-from config_manager import ConfigManager
-from daq_interface import DAQInterface
+from .config_manager import ConfigManager  # Relative import
+from .daq_interface import DAQInterface   # Relative import
 import pyqtgraph as pg
 import time
-
-class PlotWidget(QtWidgets.QWidget):
-    def __init__(self, title, channels, parent=None):
-        super().__init__(parent)
-        self.channels = channels
-        self.channel_ids = list(channels.keys())
-        self.max_points = 50
-        self.update_interval = 0.1
-        
-        self.data = {ch: [] for ch in self.channel_ids}
-        self.times = []
-        self.start_time = None
-        
-        layout = QtWidgets.QVBoxLayout()
-        self.setLayout(layout)
-
-        self.plot_widget = pg.PlotWidget(title=title)
-        self.plot_widget.setBackground('k')
-        self.plot_widget.showGrid(x=True, y=True, alpha=0.3)
-        self.plot_widget.setLabel('bottom', 'Time (s)')
-        layout.addWidget(self.plot_widget)
-        
-        self.colors = ['r', 'g', 'b', 'y', 'c', 'm', 'w']
-        
-        self.plot_widget.addLegend()
-        self.curves = {}
-        for i, (ch_id, ch_info) in enumerate(channels.items()):
-            color = self.colors[i % len(self.colors)]
-            self.curves[ch_id] = self.plot_widget.plot(pen=pg.mkPen(color, width=2), 
-                                                       name=ch_info['name'])
-        
-        self.plot_widget.setXRange(0, self.max_points * self.update_interval, padding=0)
-        self.plot_widget.enableAutoRange('y', True)
-
-    def update_plot(self, new_data):
-        if new_data is None:
-            print("No valid data received. Plot update skipped.")
-            return
-        
-        if self.start_time is None:
-            self.start_time = time.time()
-        
-        current_time = time.time() - self.start_time
-        
-        self.times.append(current_time)
-        for ch_id in self.channel_ids:
-            self.data[ch_id].append(new_data[ch_id])
-        
-        if len(self.times) > self.max_points:
-            self.times.pop(0)
-            for ch_id in self.channel_ids:
-                self.data[ch_id].pop(0)
-        
-        for ch_id in self.channel_ids:
-            self.curves[ch_id].setData(self.times, self.data[ch_id])
-            print(f"Updating {ch_id}: time={self.times[-5:]}, y={self.data[ch_id][-5:]}")
-        
-        if self.times:
-            latest_time = self.times[-1]
-            self.plot_widget.setXRange(max(0, latest_time - (self.max_points * self.update_interval)), 
-                                     latest_time, padding=0)
-        
-        self.plot_widget.repaint()
+from .plot_widget import PlotWidget       # Relative import
 
 class DAQReaderApp(QtWidgets.QMainWindow):
     def __init__(self):
@@ -76,21 +14,18 @@ class DAQReaderApp(QtWidgets.QMainWindow):
 
         self.config = ConfigManager()
         self.daq = DAQInterface()
-        self.measuring = False  # Track measuring state
+        self.measuring = False
         
         self.setup_ui()
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_plots)
-        # Timer starts stopped; activated by "Start Measuring"
 
     def setup_ui(self):
-        # Central widget with layout
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
-        main_layout = QtWidgets.QVBoxLayout(central_widget)  # Main vertical layout
+        main_layout = QtWidgets.QVBoxLayout(central_widget)
 
-        # Control panel for buttons
         control_panel = QtWidgets.QHBoxLayout()
         self.start_button = QtWidgets.QPushButton("Start Measuring")
         self.start_button.clicked.connect(self.start_measuring)
@@ -98,17 +33,15 @@ class DAQReaderApp(QtWidgets.QMainWindow):
 
         self.stop_button = QtWidgets.QPushButton("Stop Measuring")
         self.stop_button.clicked.connect(self.stop_measuring)
-        self.stop_button.setEnabled(False)  # Disabled until started
+        self.stop_button.setEnabled(False)
         control_panel.addWidget(self.stop_button)
 
         main_layout.addLayout(control_panel)
 
-        # Plot area
         self.plot_container = QtWidgets.QWidget()
         self.plot_layout = QtWidgets.QGridLayout(self.plot_container)
         main_layout.addWidget(self.plot_container)
 
-        # Menu bar
         menubar = self.menuBar()
         file_menu = menubar.addMenu('File')
         load_action = QtWidgets.QAction('Load Config', self)
@@ -128,14 +61,14 @@ class DAQReaderApp(QtWidgets.QMainWindow):
             QtWidgets.QMessageBox.warning(self, "No Config", "Please load a configuration file first.")
             return
         self.measuring = True
-        self.timer.start(100)  # Start timer at 100ms interval
+        self.timer.start(100)
         self.start_button.setEnabled(False)
         self.stop_button.setEnabled(True)
         print("Started measuring")
 
     def stop_measuring(self):
         self.measuring = False
-        self.timer.stop()  # Stop the timer
+        self.timer.stop()
         self.start_button.setEnabled(True)
         self.stop_button.setEnabled(False)
         print("Stopped measuring")
